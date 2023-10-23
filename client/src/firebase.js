9bec52b12112231fd82fa7b2d8b969754dec34f4
udaygmarathi <udaygmarathi5430@gmail.com>
2023-10-23 21:39:48 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjK2tycaxt8J8O53DYOHCtMD_p1rYmJ2Q",
+  authDomain: "mern-estate-9e37e.firebaseapp.com",
+  projectId: "mern-estate-9e37e",
+  storageBucket: "mern-estate-9e37e.appspot.com",
+  messagingSenderId: "576144829966",
+  appId: "1:576144829966:web:fe886e8cf05e20287a165f"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
